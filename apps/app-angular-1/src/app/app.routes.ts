51a1210e0923b77026f3e7ac1c81342b24587ae4
Pavel Balashov <pavel.balashov1@gmail.com>
2024-08-14 15:25:58 +0200

    feat: add carousel view to routes

--- apps/app-angular-1/src/app/app.routes.ts
+++ apps/app-angular-1/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { CarouselViewComponent } from '../views/carousel/carousel.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: CarouselViewComponent,
+  },
+];

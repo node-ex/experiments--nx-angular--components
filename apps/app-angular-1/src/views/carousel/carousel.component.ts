c7cd8db4c1b6343f1300abd6ca56171b1ddc10b0
Pavel Balashov <pavel.balashov1@gmail.com>
2024-08-15 20:49:05 +0200

    feat: add mock content for the carousel sections

--- apps/app-angular-1/src/views/carousel/carousel.component.ts
+++ apps/app-angular-1/src/views/carousel/carousel.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -9,4 +9,42 @@ import { Component, inject } from '@angular/core';
   imports: [CommonModule],
   providers: [],
 })
-export class CarouselViewComponent {}
+export class CarouselViewComponent {
+  sections: CarouselSections[] = [
+    {
+      title: 'Lorem ipsum',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem sed libero rutrum consectetur hendrerit nec lectus. Vivamus nec ornare ex, sed pretium neque. Integer nec dui rhoncus, fringilla libero vitae, aliquam neque. Integer sollicitudin nibh nec sollicitudin tempor. Cras.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed condimentum dolor. Vestibulum a risus ac ex molestie pellentesque ut sed tortor. Vivamus neque.',
+    },
+    {
+      title: 'Lorem ipsum dolor',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet, sem ullamcorper porta euismod, justo neque lacinia leo, et tempor.',
+    },
+    {
+      title: 'Lorem ipsum',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem sed libero rutrum consectetur hendrerit nec lectus. Vivamus nec ornare ex, sed pretium neque. Integer nec dui rhoncus, fringilla libero vitae, aliquam neque. Integer sollicitudin nibh nec sollicitudin tempor. Cras.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed condimentum dolor. Vestibulum a risus ac ex molestie pellentesque ut sed tortor. Vivamus neque.',
+    },
+    {
+      title: 'Lorem ipsum dolor',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet, sem ullamcorper porta euismod, justo neque lacinia leo, et tempor.',
+    },
+  ];
+}
+
+interface CarouselSections {
+  title: string;
+  description: string;
+}
